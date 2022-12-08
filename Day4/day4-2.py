6b2bee9b5bc3f2e3527d9142f42ed558e5e26435
lukasbrolin <lukas.brolin@live.se>
2022-12-08 21:35:41 +0100

    d4

--- Day4/day4-2.py
+++ Day4/day4-2.py
@@ -5,8 +5,7 @@ def count():
     with open(INPUT_FILE, 'r') as file:
                 for line in file:
                     f, s = [(list(map(int,l.split('-')))) for l in line.strip('\n').split(',')]
-                    if((f[0]<=s[0]<=f[1])|(f[0]<=s[1]<=f[1])|(s[0]<=f[0]<=s[1])|(s[0]<=f[1]<=s[1])):
-                        c+=1
+                    if((f[0]<=s[0]<=f[1])|(f[0]<=s[1]<=f[1])|(s[0]<=f[0]<=s[1])|(s[0]<=f[1]<=s[1])):c+=1
     return(c)
 
 print(count())
